6d9530d991c53de65eeb245c309ef1f99a42605d
Harold <power0721@gmail.com>
2023-01-15 15:25:59 +0800

    index paths

--- web-ui/src/views/IndexView.vue
+++ web-ui/src/views/IndexView.vue
@@ -1,37 +1,46 @@
 <template>
   <div>
-    <el-form :model="indexRequest">
+    <el-form :model="form">
       <el-form-item label="站点" :label-width="labelWidth">
-        <el-select v-model="indexRequest.siteId">
+        <el-select v-model="form.siteId">
           <el-option :label="site.name" :value="site.id" v-for="site of sites"/>
         </el-select>
       </el-form-item>
       <el-form-item label="索引名称" :label-width="labelWidth">
         <span>用于生成文件名，自动添加后缀.txt</span>
-        <el-input v-model="indexRequest.indexName" autocomplete="off"/>
+        <el-input v-model="form.indexName" autocomplete="off"/>
       </el-form-item>
-      <el-form-item label="索引路径" :label-width="labelWidth">
-        <span>支持多个路径，逗号分割</span>
-        <el-input v-model="indexRequest.paths" autocomplete="off" placeholder="逗号分割"/>
+      <el-form-item v-for="(item, index) in form.paths" :key="item.key" :label="'索引路径'+(index+1)"
+                    :label-width="labelWidth">
+        <el-input v-model="item.value">
+          <template #append>
+            <el-button class="mt-2" @click.prevent="removePath(item)">删除</el-button>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item :label-width="labelWidth">
+        <el-button @click="addPath">添加路径</el-button>
       </el-form-item>
       <el-form-item label="排除路径" :label-width="labelWidth">
         <span>支持多个路径，逗号分割</span>
-        <el-input v-model="indexRequest.excludes" autocomplete="off" placeholder="逗号分割"/>
+        <el-input v-model="form.excludes" autocomplete="off" placeholder="逗号分割"/>
       </el-form-item>
       <el-form-item label="排除关键词" :label-width="labelWidth">
         <span>支持多个关键词，逗号分割</span>
-        <el-input v-model="indexRequest.stopWords" autocomplete="off" placeholder="逗号分割"/>
+        <el-input v-model="form.stopWords" autocomplete="off" placeholder="逗号分割"/>
       </el-form-item>
       <el-form-item label="排除外部AList站点？" :label-width="labelWidth">
-        <el-switch v-model="indexRequest.excludeExternal"/>
+        <el-switch v-model="form.excludeExternal"/>
       </el-form-item>
       <el-form-item label="压缩文件？" :label-width="labelWidth">
-        <el-switch v-model="indexRequest.compress"/>
+        <el-switch v-model="form.compress"/>
       </el-form-item>
       <el-form-item label="最大索引目录层级" :label-width="labelWidth">
-        <el-input-number v-model="indexRequest.maxDepth" :min="1"/>
+        <el-input-number v-model="form.maxDepth" :min="1"/>
       </el-form-item>
-      <el-button type="primary" @click="handleIndexRequest">开始索引</el-button>
+      <el-form-item>
+        <el-button type="primary" @click="handleform">开始索引</el-button>
+      </el-form-item>
     </el-form>
     <div class="space"></div>
 
@@ -81,13 +90,17 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
+import {onMounted, reactive, ref} from 'vue'
 import axios from "axios";
-import {ElMessage} from "element-plus";
 import type {Site} from "@/model/Site";
 import type {TaskPage} from "@/model/Page";
 import type {Task} from "@/model/Task";
 import {onUnmounted} from "@vue/runtime-core";
+
+interface Item {
+  key: number
+  value: string
+}
 
 let intervalId = 0
 const labelWidth = 160
@@ -97,13 +110,16 @@ const dialogVisible = ref(false)
 const sites = ref([] as Site[])
 const tasks = ref({} as TaskPage)
 const task = ref({} as Task)
-const indexRequest = ref({
+const form = reactive({
   siteId: 0,
   indexName: 'index',
   excludeExternal: false,
   compress: false,
   maxDepth: 10,
-  paths: '/',
+  paths: [{
+    key: 1,
+    value: '/',
+  }],
   stopWords: '',
   excludes: '',
 })
@@ -112,7 +128,7 @@ const loadSites = () => {
   axios.get('/sites').then(({data}) => {
     sites.value = data
     if (sites.value && sites.value.length > 0) {
-      indexRequest.value.siteId = sites.value[0].id
+      form.siteId = sites.value[0].id
     }
   })
 }
@@ -146,16 +162,33 @@ const handleDelete = (data: any) => {
   })
 }
 
-const handleIndexRequest = () => {
+const addPath = () => {
+  form.paths.push({
+    key: Date.now(),
+    value: '',
+  })
+}
+
+const removePath = (item: Item) => {
+  if (form.paths.length < 2) {
+    return
+  }
+  const index = form.paths.indexOf(item)
+  if (index !== -1) {
+    form.paths.splice(index, 1)
+  }
+}
+
+const handleform = () => {
   const request = {
-    siteId: indexRequest.value.siteId,
-    indexName: indexRequest.value.indexName,
-    excludeExternal: indexRequest.value.excludeExternal,
-    compress: indexRequest.value.compress,
-    maxDepth: indexRequest.value.maxDepth,
-    paths: indexRequest.value.paths ? indexRequest.value.paths.split(/\s*,\s*/) : [],
-    stopWords: indexRequest.value.stopWords ? indexRequest.value.stopWords.split(/\s*,\s*/) : [],
-    excludes: indexRequest.value.excludes ? indexRequest.value.excludes.split(/\s*,\s*/) : [],
+    siteId: form.siteId,
+    indexName: form.indexName,
+    excludeExternal: form.excludeExternal,
+    compress: form.compress,
+    maxDepth: form.maxDepth,
+    paths: form.paths.map(e => e.value).filter(e => e),
+    stopWords: form.stopWords ? form.stopWords.split(/\s*,\s*/) : [],
+    excludes: form.excludes ? form.excludes.split(/\s*,\s*/) : [],
   }
   axios.post('/index', request).then(() => {
     currentPage.value = 1
