7abc4c906065a6b4b82762900424e62ba018b247
Harold <power0721@gmail.com>
2023-06-09 20:31:24 +0800

    update UI

--- web-ui/src/views/VodView.vue
+++ web-ui/src/views/VodView.vue
@@ -5,6 +5,16 @@
       <a :href="currentUrl+'/vod'" target="_blank">{{currentUrl}}/vod</a>
     </div>
 
+    <div>
+      <el-input v-model="id" autocomplete="off"/>
+      <el-button type="primary" @click="getDetail">详情</el-button>
+    </div>
+
+    <div>
+      <el-input v-model="path" autocomplete="off"/>
+      <el-button type="primary" @click="load">加载</el-button>
+    </div>
+
     <h2>API返回数据</h2>
     <div class="data">
       <pre><code>{{config}}</code></pre>
@@ -16,8 +26,22 @@
 import {onMounted, ref} from 'vue'
 import axios from "axios"
 
+const id = ref('')
+const path = ref('')
 const config = ref('')
 const currentUrl = window.location.origin
+
+const getDetail = function () {
+  axios.get('/vod?ids=' + id.value).then(({data}) => {
+    config.value = data
+  })
+}
+
+const load = function () {
+  axios.get('/vod?t=' + path.value).then(({data}) => {
+    config.value = data
+  })
+}
 
 onMounted(() => {
   axios.get('/vod').then(({data}) => {
