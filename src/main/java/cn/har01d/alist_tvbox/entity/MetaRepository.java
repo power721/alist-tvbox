193ed9d3adb4a3cc77c7873aa43100409b8a6ab7
Harold <power0721@gmail.com>
2023-07-11 13:56:07 +0800

    vod mode

--- src/main/java/cn/har01d/alist_tvbox/entity/MetaRepository.java
+++ src/main/java/cn/har01d/alist_tvbox/entity/MetaRepository.java
@@ -1,7 +1,10 @@
 package cn.har01d.alist_tvbox.entity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MetaRepository extends JpaRepository<Meta, Integer> {
     Meta findByPath(String path);
+    Page<Meta> findByPathStartsWith(String prefix, Pageable pageable);
 }
