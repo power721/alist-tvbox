3cfa8b59048df8a46ac2ba349e1b0b105d10587f
Harold <power0721@gmail.com>
2023-07-16 07:43:18 +0800

    支持B站

--- src/main/java/cn/har01d/alist_tvbox/util/Constants.java
+++ src/main/java/cn/har01d/alist_tvbox/util/Constants.java
@@ -39,5 +39,6 @@ public final class Constants {
     public static final String DOCKER_VERSION = "docker_version";
     public static final String APP_VERSION = "app_version";
     public static final String BILIBILI_COOKIE = "bilibili_cookie";
+    public static final String BILI_BILI = "BiliBili";
     public static final String ALI_SECRET = "ali_secret";
 }
