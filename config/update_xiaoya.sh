bc5e9254b8c0e03e7f3807aeb69d8b4fb6b12785
Harold <power0721@gmail.com>
2023-07-20 14:54:45 +0800

    支持B站-dash视频

--- config/update_xiaoya.sh
+++ config/update_xiaoya.sh
@@ -1,6 +1,12 @@
 BASE_DIR=/etc/xiaoya
 PORT1=4567
 PORT2=5344
+TAG="latest"
+
+if [ "$1" = "-t" ]; then
+  TAG="$2"
+  shift 2
+fi
 
 if [ $# -gt 0 ]; then
 	BASE_DIR=$1
@@ -38,24 +44,23 @@ fi
 docker image prune -f
 
 platform="linux/amd64"
-tag="latest"
 ARCH=$(uname -m)
 if [ "$ARCH" = "armv7l" ]; then
-  platform="linux/arm/v7"
-  tag="arm-v7"
+  echo "不支持的平台"
+  exit 1
 elif [ "$ARCH" = "aarch64" ]; then
     platform="linux/arm64"
 fi
 
-echo -e "\e[32m下载最新Docker镜像，平台：${platform}\e[0m"
+echo -e "\e[32m下载最新Docker镜像，平台：${platform}, image tag: ${TAG}\e[0m"
 for i in 1 2 3 4 5
 do
-   docker pull --platform ${platform} haroldli/xiaoya-tvbox:${tag} && break
+   docker pull --platform ${platform} haroldli/xiaoya-tvbox:${TAG} && break
 done
 
 echo -e "\e[33m重启应用\e[0m"
 docker rm -f xiaoya-tvbox 2>/dev/null && \
-docker run -d -p $PORT1:4567 -p $PORT2:80 -e ALIST_PORT=$PORT2 -v "$BASE_DIR":/data --restart=always --name=xiaoya-tvbox haroldli/xiaoya-tvbox:${tag}
+docker run -d -p $PORT1:4567 -p $PORT2:80 -e ALIST_PORT=$PORT2 -v "$BASE_DIR":/data --restart=always --name=xiaoya-tvbox haroldli/xiaoya-tvbox:${TAG}
 
 echo -e "\n\e[32m请使用以下命令查看日志输出：\e[0m"
 echo -e "    docker logs -f xiaoya-tvbox\n"
@@ -67,6 +72,13 @@ if [ -n "$IP" ]; then
   echo -e "    \e[32m管理界面\e[0m： http://$IP:$PORT1/"
   echo -e "    \e[32m小雅AList\e[0m： http://$IP:$PORT2/"
 else
+  IP=$(ip a | grep -F '10.' | awk '{print $2}' | awk -F/ '{print $1}' | grep -E '\b10.' | head -1)
+  if [ -n "$IP" ]; then
+    echo ""
+    echo -e "\e[32m请用以下地址访问：\e[0m"
+    echo -e "    \e[32m管理界面\e[0m： http://$IP:$PORT1/"
+    echo -e "    \e[32m小雅AList\e[0m： http://$IP:$PORT2/"
+  fi
   echo -e "\e[32m云服务器请用公网IP访问\e[0m"
 fi
 echo ""
