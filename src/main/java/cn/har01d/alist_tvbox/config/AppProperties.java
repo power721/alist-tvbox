b2a1e8cbe33e662210718ed4f7816d669fa8f2ce
Harold <power0721@gmail.com>
2023-07-28 11:47:19 +0800

    高级设置：订阅替换阿里token地址

--- src/main/java/cn/har01d/alist_tvbox/config/AppProperties.java
+++ src/main/java/cn/har01d/alist_tvbox/config/AppProperties.java
@@ -17,6 +17,7 @@ public class AppProperties {
     private boolean sort;
     private boolean merge;
     private boolean mix;
+    private boolean replaceAliToken;
     private boolean searchable;
     private int pageSize = 100;
     private int maxSearchResult = 100;
