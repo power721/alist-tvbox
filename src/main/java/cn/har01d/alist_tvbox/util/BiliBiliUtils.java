be08ddbd96aac67f17afaf8fc49390d337c64760
Harold <power0721@gmail.com>
2023-07-30 14:35:39 +0800

    内置B站账号

--- src/main/java/cn/har01d/alist_tvbox/util/BiliBiliUtils.java
+++ src/main/java/cn/har01d/alist_tvbox/util/BiliBiliUtils.java
@@ -1,9 +1,11 @@
 package cn.har01d.alist_tvbox.util;
 
-public final class BiliBiliUtils {
-    static {
-        System.loadLibrary("bilibili");
-    }
+import java.util.Base64;
 
-    public static native String getCookie();
+public final class BiliBiliUtils {
+    private static final String COOKIE = "11t9VCKbJ5jxU0VTU0RBVEE9NjYzNmJmYjIlMkMxNzA2MjQ2NzUwJTJDYjcyMzIlMkE3MXZrRmtGVlBBZEJZZUNQdkhIRVlIVEFCWmFDMzJBclBISlk5RllmalhJU0w2SXo0Yk1jUnAwTWVkV3lNNEV3b1ZuRFJ2MEFBQVJnQTtiaWxpX2pjdD0yNTllOTMwMmM2ODk4NGQ0ZWMxMGI3YjdiMGY5YmRiZDtEZWRlVXNlcklEPTE3ODQ4MDU3NzA7RGVkZVVzZXJJRF9fY2tNZDU9YThlNWQ4YTg0OGYyZDg4ZTtzaWQ9N2plNG9pb2M=";
+
+    public static String getCookie() {
+        return new String(Base64.getDecoder().decode(COOKIE.substring(12).getBytes()));
+    }
 }
