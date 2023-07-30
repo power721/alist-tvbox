bc9f41a3b614798e5be3c3d6864aa453b9b70e7c
Harold <power0721@gmail.com>
2023-07-30 21:16:35 +0800

    native image

--- src/main/java/cn/har01d/alist_tvbox/config/RestErrorHandler.java
+++ src/main/java/cn/har01d/alist_tvbox/config/RestErrorHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -14,6 +15,9 @@ public class RestErrorHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
         log.warn("", ex);
+        if (ex instanceof HttpMediaTypeException) {
+            log.warn("body: {} {}, supportedMediaTypes: {}", ((HttpMediaTypeException) ex).getBody(), ((HttpMediaTypeException) ex).getSupportedMediaTypes());
+        }
         return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 }
