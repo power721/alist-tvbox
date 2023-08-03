aeaf4890f7053a877ddecee4bc6e8dc09da2d684
Harold <power0721@gmail.com>
2023-08-03 19:21:06 +0800

    native image

--- src/main/java/cn/har01d/alist_tvbox/config/MvcConfig.java
+++ src/main/java/cn/har01d/alist_tvbox/config/MvcConfig.java
@@ -1,12 +1,10 @@
 package cn.har01d.alist_tvbox.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
