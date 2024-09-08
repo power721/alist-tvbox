e242e5918cdc19faac8fa877f963049390bd4625
Har01d <power0721@gmail.com>
2024-09-08 15:40:11 +0800

    优化Emby：排序

--- src/main/java/cn/har01d/alist_tvbox/entity/Emby.java
+++ src/main/java/cn/har01d/alist_tvbox/entity/Emby.java
@@ -1,10 +1,6 @@
 package cn.har01d.alist_tvbox.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.TableGenerator;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -21,6 +17,8 @@ public class Emby {
     @GeneratedValue(strategy = GenerationType.TABLE, generator = "tableGenerator")
     private Integer id;
 
+    @Column(name = "`order`")
+    private Integer order;
     private String name;
     private String url;
     private String username = "";
