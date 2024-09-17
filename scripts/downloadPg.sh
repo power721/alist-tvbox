f6fde599f87985839ca812b416083e11e071db86
Har01d <power0721@gmail.com>
2024-09-17 13:56:04 +0800

    自动更新pg包

--- scripts/downloadPg.sh
+++ scripts/downloadPg.sh
@@ -9,11 +9,14 @@ REMOTE=$(curl -s https://raw.githubusercontent.com/power721/pg/refs/heads/main/v
 
 echo "local PG: ${LOCAL}, remote PG: ${REMOTE}"
 if [ "$LOCAL" = "${REMOTE}" ]; then
+  echo "sync files"
   rm -rf /www/pg/* && unzip -q -o /data/pg.zip -d /www/pg && cp -r /data/pg/* /www/pg/
   exit
 fi
 
 echo "download ${REMOTE}" && \
 wget https://github.com/power721/pg/raw/refs/heads/main/pg.zip -O /data/pg.zip && \
+echo "sync files" && \
 rm -rf /www/pg/* && unzip -q -o /data/pg.zip -d /www/pg && cp -r /data/pg/* /www/pg/ && \
+echo "save version" && \
 echo -n ${REMOTE} > /data/pg_version.txt
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,7 +7,7 @@ if [ -f /data/proxy.txt ]; then
   export no_proxy="*.aliyundrive.com"
 fi
 
-chmod a+x /init.sh /index.sh
+chmod a+x /init.sh /index.sh /downloadPg.sh
 
 if [ ! -d /data/log ]; then
   mkdir -p /data/log
@@ -15,5 +15,7 @@ fi
 ln -sf /data/log /opt/atv/log
 
 /init.sh 2>&1 | tee /opt/atv/log/init.log 2>&1
+
+/downloadPg.sh 2>&1 | tee -a /opt/atv/log/init.log 2>&1
 
 java "$MEM_OPT" -cp BOOT-INF/classes:BOOT-INF/lib/* cn.har01d.alist_tvbox.AListApplication "$@"
