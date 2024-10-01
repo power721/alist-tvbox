57a9be05ab6150dcaf2637e5a2b62eb049767656
Har01d <power0721@gmail.com>
2024-10-01 21:50:23 +0800

    验证JSON

--- src/main/java/cn/har01d/alist_tvbox/web/SubscriptionController.java
+++ src/main/java/cn/har01d/alist_tvbox/web/SubscriptionController.java
@@ -7,7 +7,13 @@ import cn.har01d.alist_tvbox.service.SubscriptionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
