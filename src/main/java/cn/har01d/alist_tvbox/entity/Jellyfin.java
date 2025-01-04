d4630be83604cb9405fe1f133297894e3ada7d49
Harold <power0721@gmail.com>
2025-01-04 13:01:33 +0800

    支持Jellyfin站点 (#549)

---
+++ src/main/java/cn/har01d/alist_tvbox/entity/Jellyfin.java
@@ -0,0 +1,32 @@
+package cn.har01d.alist_tvbox.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.TableGenerator;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@TableGenerator(name = "tableGenerator", table = "id_generator", pkColumnName = "entity_name", valueColumnName = "next_id", allocationSize = 1)
+public class Jellyfin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tableGenerator")
+    private Integer id;
+
+    @Column(name = "`order`")
+    private Integer order;
+    private String name;
+    private String url;
+    private String userAgent;
+    private String username = "";
+    private String password = "";
+}
---
+++ web-ui/src/views/JellyfinView.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="sites">
+    <h1>Jellyfin站点列表</h1>
+    <el-row justify="end">
+      <el-button @click="load">刷新</el-button>
+      <el-button type="primary" @click="handleAdd">添加</el-button>
+    </el-row>
+    <div class="space"></div>
+
+    <el-table :data="sites" border style="width: 100%">
+<!--      <el-table-column prop="id" label="ID" sortable width="70"/>-->
+      <el-table-column prop="name" label="名称" sortable width="180"/>
+      <el-table-column prop="order" label="顺序" sortable width="90"/>
+      <el-table-column prop="username" label="用户名" sortable width="180"/>
+      <el-table-column prop="url" label="URL地址" sortable>
+        <template #default="scope">
+          <a :href="scope.row.url" target="_blank">{{ scope.row.url }}</a>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="200">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button link type="danger" size="small" @click="handleDelete(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog v-model="formVisible" :title="dialogTitle">
+      <el-form :model="form">
+        <el-form-item label="名称" label-width="140" required>
+          <el-input v-model="form.name" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="URL地址" label-width="140" required>
+          <el-input v-model="form.url" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="User Agent" label-width="140">
+          <el-input v-model="form.userAgent" type="textarea" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="用户名" label-width="140" required>
+          <el-input v-model="form.username" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="密码" label-width="140">
+          <el-input v-model="form.password" type="password" show-password autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="顺序" label-width="140">
+          <el-input-number v-model="form.order" :min="0"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleCancel">取消</el-button>
+        <el-button type="primary" @click="handleConfirm">{{ updateAction ? '更新' : '添加' }}</el-button>
+      </span>
+      </template>
+    </el-dialog>
+
+    <el-dialog v-model="dialogVisible" title="删除Jellyfin站点" width="30%">
+      <p>是否删除Jellyfin站点 - {{ form.name }}</p>
+      <p>{{ form.url }}</p>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="danger" @click="deleteSite">删除</el-button>
+      </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref} from 'vue'
+import {Check, Close, Refresh} from '@element-plus/icons-vue'
+import axios from "axios"
+
+const updateAction = ref(false)
+const dialogTitle = ref('')
+const sites = ref([])
+const formVisible = ref(false)
+const dialogVisible = ref(false)
+const form = ref({
+  id: 0,
+  name: '',
+  url: '',
+  userAgent: '',
+  username: '',
+  password: '',
+  order: 0,
+})
+
+const handleAdd = () => {
+  dialogTitle.value = '添加Jellyfin站点'
+  updateAction.value = false
+  form.value = {
+    id: 0,
+    name: '',
+    url: '',
+    userAgent: '',
+    username: '',
+    password: '',
+    order: 0,
+  }
+  formVisible.value = true
+}
+
+const handleEdit = (data: any) => {
+  dialogTitle.value = '更新Jellyfin站点 - ' + data.name
+  updateAction.value = true
+  form.value = Object.assign({}, data)
+  formVisible.value = true
+}
+
+const handleDelete = (data: any) => {
+  form.value = data
+  dialogVisible.value = true
+}
+
+const deleteSite = () => {
+  dialogVisible.value = false
+  axios.delete('/api/jellyfin/' + form.value.id).then(() => {
+    load()
+  })
+}
+
+const handleCancel = () => {
+  formVisible.value = false
+}
+
+const handleConfirm = () => {
+  const url = updateAction.value ? '/api/jellyfin/' + form.value.id : '/api/jellyfin'
+  axios.post(url, form.value).then(() => {
+    formVisible.value = false
+    load()
+  })
+}
+
+const load = () => {
+  axios.get('/api/jellyfin').then(({data}) => {
+    sites.value = data
+  })
+}
+
+onMounted(() => {
+  load()
+})
+</script>
+
+<style scoped>
+.space {
+  margin-bottom: 6px;
+}
+</style>
