7cf4194c568b8eadefc2c83deb3abab8c9593e06
Har01d <power0721@gmail.com>
2025-04-26 12:53:59 +0800

    修复索引数据下载

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,8 +1,8 @@
 if [ $# -gt 0 ]; then
   remote=$1
-  wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppelWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36" -T 30 -t 2 http://docker.xiaoya.pro/update/index.zip || \
-  wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppelWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36" --header="Host:docker.xiaoya.pro" -T 40 -t 2 http://104.21.17.247/update/index.zip -O index.zip || \
-  wget -t 3 http://har01d.org/index.zip -O index.zip
+  wget -t 2 https://gh-proxy.net/https://raw.githubusercontent.com/xiaoyaliu00/data/main/index.zip || \
+  wget -t 2 https://raw.githubusercontent.com/xiaoyaliu00/data/main/index.zip -O index.zip || \
+  wget -t 2 http://har01d.org/index.zip -O index.zip
   if [ ! -f index.zip ]; then
     echo "Failed to download index compressed file, the index file upgrade process has aborted"
     exit 1
