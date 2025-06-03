aec1f9c379df42b0322e3c3306f2b3d6b0b19ac6
Har01d <power0721@gmail.com>
2025-06-03 21:15:05 +0800

    fix sql

--- src/main/java/cn/har01d/alist_tvbox/storage/UC.java
+++ src/main/java/cn/har01d/alist_tvbox/storage/UC.java
@@ -8,6 +8,7 @@ public class UC extends Storage {
         setWebProxy(account.isUseProxy());
         setWebdavPolicy("native_proxy");
         addAddition("cookie", account.getCookie());
+        addAddition("token", "Nk27FcCv6q1eo6rXz8QHR/nIG6qLA3jh7KdL+agFgcOvww==");
         addAddition("root_folder_id", account.getFolder());
         addAddition("order_by", "file_name");
         addAddition("order_direction", "asc");
