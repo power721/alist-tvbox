338e9b454e66969f717b436136073314f27deb4b
Har01d <power0721@gmail.com>
2025-06-03 20:43:02 +0800

    fix sql

--- src/main/java/cn/har01d/alist_tvbox/storage/Pan115Share.java
+++ src/main/java/cn/har01d/alist_tvbox/storage/Pan115Share.java
@@ -5,11 +5,11 @@ import cn.har01d.alist_tvbox.entity.Share;
 public class Pan115Share extends Storage {
     public Pan115Share(Share share) {
         super(share, "115 Share");
-        addAddition("share_code", share.getShareId());
-        addAddition("receive_code", share.getPassword());
+        addAddition("share_id", share.getShareId());
+        addAddition("share_pwd", share.getPassword());
         addAddition("root_folder_id", share.getFolderId());
         addAddition("page_size", 1500);
-        addAddition("limit_rate", 1);
+        addAddition("limit_rate", 2);
         buildAddition();
     }
 }
