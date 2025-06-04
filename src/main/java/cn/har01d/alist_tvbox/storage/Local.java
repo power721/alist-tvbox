78463ef02d2b69940f7c7c07bf329eb14b434984
power721 <power0721@gmail.com>
2025-06-04 09:54:15 +0800

    Fix local (#720)

--- src/main/java/cn/har01d/alist_tvbox/storage/Local.java
+++ src/main/java/cn/har01d/alist_tvbox/storage/Local.java
@@ -5,13 +5,9 @@ import cn.har01d.alist_tvbox.entity.Share;
 public class Local extends Storage {
     public Local(Share share) {
         super(share, "Local");
-        setCacheExpiration(0);
         setWebdavPolicy("native_proxy");
-        addAddition("root_folder_id", share.getFolderId());
+        addAddition("root_folder_path", share.getFolderId());
         addAddition("thumbnail", false);
-        addAddition("show_hidden", true);
-        addAddition("mkdir_perm", "777");
-        addAddition("thumb_cache_folder", "");
         buildAddition();
     }
 }
