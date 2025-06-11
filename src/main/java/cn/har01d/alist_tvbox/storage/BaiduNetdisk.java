7977e4358d9b8a25d2126e7d9a59e6faf88b77df
Har01d <power0721@gmail.com>
2025-06-11 16:07:42 +0800

    百度获取认证令牌

--- src/main/java/cn/har01d/alist_tvbox/storage/BaiduNetdisk.java
+++ src/main/java/cn/har01d/alist_tvbox/storage/BaiduNetdisk.java
@@ -1,6 +1,7 @@
 package cn.har01d.alist_tvbox.storage;
 
 import cn.har01d.alist_tvbox.entity.DriverAccount;
+import org.apache.commons.lang3.StringUtils;
 
 public class BaiduNetdisk extends Storage {
     public BaiduNetdisk(DriverAccount account) {
@@ -11,11 +12,16 @@ public class BaiduNetdisk extends Storage {
         addAddition("concurrency", account.getConcurrency());
         addAddition("order_by", "name");
         addAddition("order_direction", "asc");
-        addAddition("client_id", "iYCeC9g08h5vuP9UqvPHKKSVrKFXGa1v");
-        addAddition("client_secret", "jXiFMOPVPCWlO2M5CwWQzffpNPaGTRBG");
+        if (StringUtils.isBlank(account.getAddition())) {
+            addAddition("client_id", "iYCeC9g08h5vuP9UqvPHKKSVrKFXGa1v");
+            addAddition("client_secret", "jXiFMOPVPCWlO2M5CwWQzffpNPaGTRBG");
+        } else {
+            addAddition("client_id", "IlLqBbU3GjQ0t46TRwFateTprHWl39zF");
+            addAddition("client_secret", "");
+        }
         addAddition("custom_crack_ua", "netdisk");
         addAddition("download_api", "crack_video");
         addAddition("only_list_video_file", true);
-        buildAddition();
+        buildAddition(account);
     }
 }
