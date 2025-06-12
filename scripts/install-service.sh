d11fc5e04bd29cf2bc25d8ca48d22fd96937b087
Har01d <power0721@gmail.com>
2025-06-13 06:26:25 +0800

    update build script

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION1=0.1.11
-VERSION2=0.1.3
+VERSION1=$(curl -s http://d.har01d.cn/app.version.txt)
+VERSION2=$(curl -s http://d.har01d.cn/alist.version.txt)
 
 LOCAL_VERSION1="0.0.0"
 LOCAL_VERSION2="0.0.0"
