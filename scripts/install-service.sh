cca186f08a3c37b32aac24cc7436d86f6b0029d7
power721 <power0721@gmail>
2025-06-12 17:40:43 +0800

    update build code

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -1,41 +1,33 @@
 #!/usr/bin/env bash
 set -e
 
-LOCAL_VERSION=""
-LOCAL_APP_VERSION=""
-VERSION=$(curl -s https://api.github.com/repos/power721/alist/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)
-APP_VERSION=$(curl -s https://api.github.com/repos/power721/alist-tvbox/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)
-USERNAME=$USER
+LOCAL_VERSION1="0.0.0"
+LOCAL_VERSION2="0.0.0"
+VERSION1=$(curl -s https://api.github.com/repos/power721/alist-tvbox/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)
+VERSION2=$(curl -s https://api.github.com/repos/power721/alist/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)
+USER=$(id -nu)
+GROUP=$(id -gn)
 APPNAME=atv
 
 if [ -f /opt/${APPNAME}/data/app_version ]; then
-  LOCAL_APP_VERSION=$(head -n 1 </opt/${APPNAME}/data/app_version)
+  LOCAL_VERSION1=$(head -n 1 </opt/${APPNAME}/data/app_version)
 fi
 
 if [ -f /opt/${APPNAME}/alist/data/version ]; then
-  LOCAL_VERSION=$(head -n 1 </opt/${APPNAME}/alist/data/version)
+  LOCAL_VERSION2=$(head -n 1 </opt/${APPNAME}/alist/data/version)
 fi
 
-if [ "$LOCAL_APP_VERSION" = "$APP_VERSION" ] && [ "$LOCAL_VERSION" = "$VERSION" ] ; then
+if [ "$LOCAL_VERSION1" = "$VERSION1" ] && [ "$LOCAL_VERSION2" = "$VERSION2" ] ; then
     echo "Already latest version"
-    echo "AList: $VERSION"
-    echo "AList TvBox: $APP_VERSION"
+    echo "AList: $VERSION2"
+    echo "AList TvBox: $VERSION1"
     exit 0
 fi
 
-sudo mkdir -p /opt/${APPNAME}/alist/data
-sudo mkdir -p /opt/${APPNAME}/alist/log
-sudo mkdir -p /opt/${APPNAME}/config
-sudo mkdir -p /opt/${APPNAME}/data/backup
-sudo mkdir -p /opt/${APPNAME}/data/atv
-sudo mkdir -p /opt/${APPNAME}/index
-sudo mkdir -p /opt/${APPNAME}/log
-sudo mkdir -p /opt/${APPNAME}/scripts
-sudo mkdir -p /opt/${APPNAME}/www/cat
-sudo mkdir -p /opt/${APPNAME}/www/pg
-sudo mkdir -p /opt/${APPNAME}/www/zx
-sudo mkdir -p /opt/${APPNAME}/www/tvbox
-sudo mkdir -p /opt/${APPNAME}/www/files
+sudo mkdir -p /opt/${APPNAME}/alist/{data,log}
+sudo mkdir -p /opt/${APPNAME}/{config,scripts,index,log}
+sudo mkdir -p /opt/${APPNAME}/data/{atv,backup}
+sudo mkdir -p /opt/${APPNAME}/www/{cat,pg,zx,tvbox,files}
 
 cat <<EOF >/tmp/${APPNAME}.yaml
 spring:
@@ -47,11 +39,19 @@ conf=/opt/${APPNAME}/config/application-production.yaml
 [ -f $conf ] || sudo mv /tmp/${APPNAME}.yaml $conf
 
 # TODO: download scripts
-# TODO: download alist
 # TODO: init alist
 
-[ "$LOCAL_APP_VERSION" = "$APP_VERSION" ] || wget https://github.com/power721/alist-tvbox/releases/download/$APP_VERSION/atv -O /tmp/${APPNAME}
-[ "$LOCAL_VERSION" = "$VERSION" ] || wget https://github.com/power721/alist/releases/download/$VERSION/alist-linux-musl-amd64.tar.gz -O /tmp/alist.tgz && tar xf /tmp/alist.tgz -C /tmp
+mkdir -p /tmp/${APPNAME}
+
+[ "$LOCAL_VERSION1" != "$VERSION1" ] && \
+echo "Upgrade atv from $LOCAL_VERSION1 to $VERSION1" && \
+wget https://github.com/power721/alist-tvbox/releases/download/$VERSION1/atv.tar.gz -O /tmp/${APPNAME}/atv.tgz && \
+tar xf /tmp/${APPNAME}/atv.tgz -C /tmp/${APPNAME}/
+
+[ "$LOCAL_VERSION2" != "$VERSION2" ] && \
+echo "Upgrade alist from $LOCAL_VERSION2 to $VERSION2" && \
+wget https://github.com/power721/alist/releases/download/$VERSION2/alist-linux-musl-amd64.tar.gz -O /tmp/${APPNAME}/alist.tgz && \
+tar xf /tmp/${APPNAME}/alist.tgz -C /tmp/${APPNAME}/
 
 cat <<EOF > /tmp/${APPNAME}.service
 [Unit]
@@ -59,7 +59,8 @@ Description=${APPNAME} API
 After=syslog.target
 
 [Service]
-User=${USERNAME}
+User=${USER}
+Group=${GROUP}
 WorkingDirectory=/opt/${APPNAME}
 ExecStart=/opt/${APPNAME}/${APPNAME} --spring.profiles.active=standalone,production
 SuccessExitStatus=143
@@ -71,12 +72,13 @@ EOF
 sudo mv /tmp/${APPNAME}.service /etc/systemd/system/${APPNAME}.service
 sudo systemctl daemon-reload
 sudo systemctl stop ${APPNAME}.service
-[ "$LOCAL_APP_VERSION" = "$APP_VERSION" ] || sudo mv /tmp/${APPNAME} /opt/${APPNAME}/${APPNAME}
-[ "$LOCAL_VERSION" = "$VERSION" ] || mv /tmp/alist /opt/${APPNAME}/alist/
-sudo chmod +x /opt/${APPNAME}/${APPNAME}
-sudo echo $APP_VERSION > /opt/${APPNAME}/data/app_version
-sudo echo $VERSION > /opt/${APPNAME}/alist/data/version
-sudo chown -R ${USERNAME}:${USERNAME} /opt/${APPNAME}
+
+[ "$LOCAL_VERSION1" != "$VERSION1" ] && sudo mv /tmp/${APPNAME}/atv /opt/${APPNAME}/${APPNAME}
+[ "$LOCAL_VERSION2" != "$VERSION2" ] && sudo mv /tmp/${APPNAME}/alist /opt/${APPNAME}/alist/alist && /opt/${APPNAME}/alist/alist admin
+sudo chmod +x /opt/${APPNAME}/${APPNAME} /opt/${APPNAME}/alist/alist
+sudo echo $VERSION1 > /opt/${APPNAME}/data/app_version
+sudo echo $VERSION2 > /opt/${APPNAME}/alist/data/version
+sudo chown -R ${USER}:${GROUP} /opt/${APPNAME}
 
 sudo systemctl enable ${APPNAME}.service
 sudo systemctl start ${APPNAME}.service
