879105ba93ca2b215b7e45f66902c63b0c0c7d05
Har01d <power0721@gmail.com>
2025-06-13 18:43:34 +0800

    修复www文件映射

--- src/main/java/cn/har01d/alist_tvbox/config/MvcConfig.java
+++ src/main/java/cn/har01d/alist_tvbox/config/MvcConfig.java
@@ -1,5 +1,7 @@
 package cn.har01d.alist_tvbox.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,13 +11,15 @@ import cn.har01d.alist_tvbox.util.Utils;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    private static final Logger log = LoggerFactory.getLogger(MvcConfig.class);
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/cat/**").addResourceLocations("file:" + Utils.getWebPath("cat"));
-        registry.addResourceHandler("/tvbox/**").addResourceLocations("file:" + Utils.getWebPath("tvbox"));
-        registry.addResourceHandler("/files/**").addResourceLocations("file:" + Utils.getWebPath("files"));
-        registry.addResourceHandler("/pg/**").addResourceLocations("file:" + Utils.getWebPath("pg"));
-        registry.addResourceHandler("/zx/**").addResourceLocations("file:" + Utils.getWebPath("zx"));
+        registry.addResourceHandler("/cat/**").addResourceLocations("file:" + Utils.getWebPath("cat") + "/");
+        registry.addResourceHandler("/tvbox/**").addResourceLocations("file:" + Utils.getWebPath("tvbox") + "/");
+        registry.addResourceHandler("/files/**").addResourceLocations("file:" + Utils.getWebPath("files") + "/");
+        registry.addResourceHandler("/pg/**").addResourceLocations("file:" + Utils.getWebPath("pg") + "/");
+        registry.addResourceHandler("/zx/**").addResourceLocations("file:" + Utils.getWebPath("zx") + "/");
     }
 
     @Override
