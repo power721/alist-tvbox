198942d74ab2c2208064f9a7e70274954894932b
power721 <power0721@gmail.com>
2025-06-15 12:05:15 +0800

    更新版本 (#773)

--- src/main/java/cn/har01d/alist_tvbox/model/LoginResponse.java
+++ src/main/java/cn/har01d/alist_tvbox/model/LoginResponse.java
@@ -1,7 +1,11 @@
 package cn.har01d.alist_tvbox.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class LoginResponse extends Response<LoginToken> {
 }
