ceff68a0b6f1858a60d8f97a187ba8a5790460c4
Har01d <power0721@gmail.com>
2025-07-02 08:23:28 +0800

    修复原生版二维码

--- build-base.sh
+++ build-base.sh
@@ -6,8 +6,5 @@ cd target
 java -Djarmode=layertools -jar alist-tvbox-1.0.jar extract
 cd ..
 
-echo "=== build haroldli/java:17 ==="
-docker build -f docker/Dockerfile-jre --tag=haroldli/java:17 .
-
 echo "=== build haroldli/alist-base ==="
 docker build -f docker/Dockerfile-base --tag=haroldli/alist-base:latest .
