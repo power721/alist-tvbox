d8a175ab8dbbff9e21cd31d0441a207cf22e4025
power721 <power0721@gmail.com>
2025-07-03 10:30:40 +0800

    support mysql

--- src/main/java/cn/har01d/alist_tvbox/service/SettingService.java
+++ src/main/java/cn/har01d/alist_tvbox/service/SettingService.java
@@ -144,8 +144,12 @@ public class SettingService {
 
     @Scheduled(cron = "0 0 6 * * *")
     public File backupDatabase() {
+        if (environment.matchesProfiles("mysql")) {
+            return null;
+        }
+
         try {
-            jdbcTemplate.execute("SCRIPT TO '/tmp/script.sql' TABLE ACCOUNT, ALIST_ALIAS, CONFIG_FILE, ID_GENERATOR, INDEX_TEMPLATE, NAVIGATION, PIK_PAK_ACCOUNT, SETTING, SHARE, SITE, SUBSCRIPTION, TASK, USERS, TMDB, TMDB_META");
+            jdbcTemplate.execute("SCRIPT TO '/tmp/script.sql' TABLE ACCOUNT, ALIST_ALIAS, CONFIG_FILE, ID_GENERATOR, INDEX_TEMPLATE, NAVIGATION, PIK_PAK_ACCOUNT, SETTING, SHARE, SITE, SUBSCRIPTION, TASK, x_user, TMDB, TMDB_META, DEVICE, DRIVER_ACCOUNT, EMBY, HISTORY, JELLYFIN, PLAY_URL, TENANT");
             File out = Utils.getDataPath("backup", "database-" + LocalDate.now() + ".zip").toFile();
             out.createNewFile();
             try (FileOutputStream fos = new FileOutputStream(out);
