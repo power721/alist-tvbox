bdab6e21dfa26ac5fd0e96cd8aee2afd84329079
power721 <power0721@gmail.com>
2025-07-04 13:54:48 +0800

    fix Dockerfile

---
+++ scripts/entrypoint-native.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -r /data/env ]; then
+    echo "Reading env file"
+    cp /data/env /etc/profile.d/custom_env.sh
+    chmod +x /etc/profile.d/custom_env.sh
+fi
+
+if [ -r /data/proxy.txt ]; then
+  proxy_url=$(head -n1 /data/proxy.txt)
+  export HTTP_PROXY=$proxy_url
+  export HTTPS_PROXY=$proxy_url
+  export no_proxy="*.aliyundrive.com"
+fi
+
+chmod a+x /*.sh
+
+if [ ! -d /data/log ]; then
+  mkdir -p /data/log
+fi
+ln -sf /data/log /opt/atv/log
+
+/init.sh 2>&1 | tee /opt/atv/log/init.log 2>&1
+
+./atv "$@"
