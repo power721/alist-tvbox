1668d3e90726e3de6280b39583c5a756c4fa0012
power721 <power0721@gmail.com>
2025-07-07 07:40:38 +0800

    修复猫影视登录 (#848)

--- web-ui/src/views/LoginView.vue
+++ web-ui/src/views/LoginView.vue
@@ -1,6 +1,16 @@
 <template>
-  <el-alert title="关于密码加强" type="warning" :description="alert" show-icon v-if="showAlert" @close="closeAlert"/>
-  <div class="space"></div>
+  <div v-if="showAlert">
+    <el-alert title="关于密码加强" type="warning" description="本次升级会强制更改帐号为随机密码。" show-icon @close="closeAlert"/>
+    <div class="alert">
+      <p>请从Docker日志搜索“密码”或者安装目录的initial_admin_credentials.txt文件查看密码：</p>
+      <p><code>cat /etc/xiaoya/initial_admin_credentials.txt</code></p>
+      <p>或者查看容器内文件：</p>
+      <p><code>docker exec -it alist-tvbox cat /data/initial_admin_credentials.txt</code></p>
+      <p><code>docker exec -it xiaoya-tvbox cat /data/initial_admin_credentials.txt</code></p>
+      <p>如果找不到密码，ssh到系统运行：<code>sudo bash -c "$(curl -fsSL http://d.har01d.cn/alist-tvbox.sh)"</code> ，选择菜单8和8，重置密码。</p>
+      <p>WebDAV使用配置页面设置的用户名和密码。</p>
+    </div>
+  </div>
 
   <el-form :model="account" :rules="rules" status-icon label-width="120px">
     <el-form-item prop="username" label="用户名">
@@ -19,12 +29,6 @@
 import {onMounted, reactive, ref} from "vue";
 import {useRoute, useRouter} from "vue-router";
 import accountService from "@/services/account.service";
-
-const alert = '本次升级会强制更改帐号为随机密码。' +
-  '请从Docker日志搜索“密码”或者数据目录的initial_admin_credentials.txt文件查看密码。' +
-  '如果找不到密码，ssh到系统运行：sudo bash -c "$(curl -fsSL http://d.har01d.cn/alist-tvbox.sh)"' +
-  ' 选择菜单8和8，重置密码。' +
-  'WebDAV使用配置页面设置的用户名和密码。'
 
 const showAlert = ref(true)
 const route = useRoute()
@@ -52,6 +56,7 @@ const login = () => {
 }
 
 const closeAlert = () => {
+  showAlert.value = false
   localStorage.setItem('password-alert', 'no')
 }
 
@@ -61,5 +66,9 @@ onMounted(() => {
 </script>
 
 <style scoped>
-
+.alert {
+  margin-left: 24px;
+  margin-top: 24px;
+  margin-bottom: 24px;
+}
 </style>
