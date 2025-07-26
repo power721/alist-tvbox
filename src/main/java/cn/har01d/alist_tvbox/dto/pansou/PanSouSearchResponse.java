a13782b2423e5ffce134bb008c366e39c0f3aebc
power721 <power0721@gmail.com>
2025-07-26 18:50:48 +0800

    Fix pansou2 (#879)

--- src/main/java/cn/har01d/alist_tvbox/dto/pansou/PanSouSearchResponse.java
+++ src/main/java/cn/har01d/alist_tvbox/dto/pansou/PanSouSearchResponse.java
@@ -3,7 +3,7 @@ package cn.har01d.alist_tvbox.dto.pansou;
 import lombok.Data;
 
 @Data
-public class PansouSearchResponse {
+public class PanSouSearchResponse {
     private Integer code;
     private String message;
     private SearchResponse data;
