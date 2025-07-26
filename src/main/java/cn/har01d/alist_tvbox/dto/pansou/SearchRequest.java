c10a7712d0115952fbab78bc71139d3c8c89e1aa
Har01d <power0721@gmail.com>
2025-07-26 18:37:49 +0800

    修复盘搜

--- src/main/java/cn/har01d/alist_tvbox/dto/pansou/SearchRequest.java
+++ src/main/java/cn/har01d/alist_tvbox/dto/pansou/SearchRequest.java
@@ -6,17 +6,8 @@ import java.util.List;
 
 @Data
 public class SearchRequest {
-    private String kw;
-    private List<String> channels;
-    private String src;
+    private final String kw;
+    private final List<String> channels;
+    private final String src;
     private String res = "results";
-
-    public SearchRequest() {
-    }
-
-    public SearchRequest(String kw, List<String> channels, String src) {
-        this.kw = kw;
-        this.channels = channels;
-        this.src = src;
-    }
 }
