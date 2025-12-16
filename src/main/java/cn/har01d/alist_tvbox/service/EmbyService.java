b8ef66143beddfde0638d992d0f4150b3ec26f7f
power721 <power0721@gmail.com>
2025-12-16 12:07:43 +0800

    upgrade spring boot (#958)

--- src/main/java/cn/har01d/alist_tvbox/service/EmbyService.java
+++ src/main/java/cn/har01d/alist_tvbox/service/EmbyService.java
@@ -624,7 +624,7 @@ public class EmbyService {
         } catch (Exception e) {
             log.warn("start playing", e);
         }
-        String playPre = emby.getUrl().contains("emos.lol") ? "/emby" : "";
+        String playPre = (emby.getUrl().contains("emos.lol") || emby.getDeviceName().contains("emos")) ? "/emby" : "";
         List<String> urls = new ArrayList<>();
         for (var source : media.getItems()) {
             urls.add(source.getName());
