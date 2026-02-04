1ea1075bbe2b9405bd9a75b246acf0270255f07e
MaxCrazy1101 <35657483+MaxCrazy1101@users.noreply.github.com>
2026-02-05 01:22:06 +0800

    webui: eslint upgrade

---
+++ web-ui/eslint.config.mjs
@@ -0,0 +1,19 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTs from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
+
+    {
+        name: 'app/files-to-ignore',
+        ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    },
+
+    ...pluginVue.configs['flat/essential'],
+    ...vueTs(),
+    skipFormatting,
+]
